8a63f424355ccf675b9653f49ef1d79e10b7741a
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-03-09 07:00:22 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -9,13 +9,13 @@ let app = express();
 console.log("Hello World");
 
 /** 2) A first working Express Server */
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
 
 /** 3) Serve an HTML file */
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 /** 4) Serve static assets  */
